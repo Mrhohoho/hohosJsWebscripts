7b7a5d411993698b6628af4a17d8fcbd74659abf
Mrhohoho <cswku@naver.com>
2024-04-03 11:42:38 +0900

    publish test.js

---
+++ publish test.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         test
+// @namespace    hoho
+// @version      2024-04-03
+// @description  hoho
+// @author       You
+// @match        http://*/*
+// @icon         
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
+
+console.log("136");
